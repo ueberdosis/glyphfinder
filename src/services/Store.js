9ce9391768a906ba2ca5e3f24e9344ac0fc2b0c5
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-21 10:02:26 +0100

    add reset db on migration
    Former-commit-id: e182760f5f228bc477b5b0fdfc3b3953236701fd

--- src/services/Store.js
+++ src/services/Store.js
@@ -1,5 +1,10 @@
+import fs from 'fs'
+import path from 'path'
 import Store from 'electron-store'
 import { app, remote } from 'electron'
+
+const userDataPath = (app || remote.app).getPath('userData')
+const dbPath = path.join(userDataPath, 'db.json')
 
 export default new Store({
   // configName: process.env.NODE_ENV === 'development'
@@ -10,6 +15,7 @@ export default new Store({
     : remote.app.getVersion(),
   migrations: {
     '1.0.0': store => {
+      fs.unlinkSync(dbPath)
       store.clear()
   ***REMOVED***,
 ***REMOVED***,
