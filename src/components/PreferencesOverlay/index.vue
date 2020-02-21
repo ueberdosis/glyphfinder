a2a3183a21581be6d1eb94ab68bb18c4d15b5d19
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-21 11:19:02 +0100

    add preferences styling
    Former-commit-id: 6cbaa78bcea5024707cba23d512e6b3e8493a2d6

--- src/components/PreferencesOverlay/index.vue
+++ src/components/PreferencesOverlay/index.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="preferences-overlay">
-    preferences
+    <div class="preferences-overlay__header">
+      <h2>
+        Preferences
+      </h2>
+      <btn icon="close" @click.native="close" />
+    </div>
 
-    <btn icon="close" @click.native="close" />
+    <div class="preferences-overlay__content">
+      <div class="preferences-overlay__section">
+        <div class="preferences-overlay__label">
+          Support
+        </div>
+        <div>
+          <a href="mailto:support@glyphfinder.com">
+            support@glyphfinder.com
+          </a>
+        </div>
+      </div>
+
+      <div class="preferences-overlay__section">
+        <div class="preferences-overlay__label">
+          License
+        </div>
+        <div>
+          Licenced to XXX
+        </div>
+      </div>
+
+      <div class="preferences-overlay__section">
+        <div class="preferences-overlay__label">
+          Danger Zone
+        </div>
+        <div>
+          <btn @click.native="clearCache" is-red>
+            Clear Cache
+          </btn>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Btn from '@/components/Btn'
+import DB from '@/services/DB'
 import Event from '@/services/Event'
+import Store from '@/services/Store'
+import Btn from '@/components/Btn'
 
 export default {
   components: {
@@ -18,6 +56,15 @@ export default {
   methods: {
     close() {
       Event.emit('hidePreferences')
+  ***REMOVED***,
+
+    clearCache() {
+      // eslint-disable-next-line
+      if (confirm('Do you really want to clear the cache?')) {
+        Store.clear()
+        DB.removeAll()
+        window.location.reload()
+    ***REMOVED***
   ***REMOVED***,
 ***REMOVED***,
 }
