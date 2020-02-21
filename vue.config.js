91f2a7e4021f77bee941ebdb8682c9d1561fa304
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-21 15:16:27 +0100

    add win config
    Former-commit-id: 716f0f378f186aca9bad6b73dfe1344d644d5cdd

--- vue.config.js
+++ vue.config.js
@@ -84,6 +84,18 @@ module.exports = {
           ***REMOVED***,
           ],
       ***REMOVED***,
+        win: {
+          publish: [
+            {
+              provider: 'spaces',
+              name: 'ueber',
+              region: 'fra1',
+              channel: 'latest',
+              path: '/glyphfinder/win',
+              acl: 'public-read',
+          ***REMOVED***,
+          ],
+      ***REMOVED***,
     ***REMOVED***,
   ***REMOVED***,
 ***REMOVED***,
