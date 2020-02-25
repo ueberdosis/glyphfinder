24968446a622de3432cb09ac35bbf93c3e81b2fa
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-25 14:23:07 +0100

    fix a bug
    Former-commit-id: 557fa26bb87adb9b64348acef756aaeb77088fca

--- src/components/Navigatable/index.vue
+++ src/components/Navigatable/index.vue
@@ -57,6 +57,7 @@ export default {
 ***REMOVED*** collect(this.usage)
         .sortByDesc('count')
         .map(item => this.formattedGlyphs.find(glyph => glyph.symbol === item.symbol))
+        .filter()
         .map(glyph => ({
           ...glyph,
           id: `frequently_used_${glyph.symbol}`,
