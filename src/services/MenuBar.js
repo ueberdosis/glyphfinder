f933593b46c677bf21be3640a50c05c7f001b1c3
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-17 14:46:57 +0100

    add more menu items to menubar
    Former-commit-id: 73a0fb1d0480882126bbee8e84c6acaa2473849e

--- src/services/MenuBar.js
+++ src/services/MenuBar.js
@@ -10,6 +10,7 @@ import {
 ***REMOVED***
 import User from './User'
 import Setapp from './Setapp'
+import Updater from './Updater'
 
 const isProduction = process.env.NODE_ENV === 'production'
 const isDevelopment = !isProduction
@@ -60,9 +61,9 @@ const isDevelopment = !isProduction
   ***REMOVED***
 
       this.menubar.on('after-create-window', () => {
-
-
         const contextMenu = Menu.buildFromTemplate([
+          { role: 'about' },
+          { type: 'separator' },
           {
             label: 'Preferences',
             click: () => {
@@ -74,6 +75,15 @@ const isDevelopment = !isProduction
             ***REMOVED***
           ***REMOVED***,
         ***REMOVED***,
+          { type: 'separator' },
+          ...(!Setapp.isActive ? [
+            {
+              label: 'Check for Updates',
+              click(menuItem) {
+                Updater.checkForUpdates(menuItem)
+            ***REMOVED***,
+          ***REMOVED***,
+          ] : []),
           { type: 'separator' },
           {
             label: 'Quit',
