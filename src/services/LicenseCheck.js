aca1cc187c69fe91c39c9e4e61632ba1e7985943
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-25 16:59:03 +0100

    log license check errors
    Former-commit-id: 834270c888e282ba3ee144fd10b1a185a800d366

--- src/services/LicenseCheck.js
+++ src/services/LicenseCheck.js
@@ -2,8 +2,11 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
 
 const masterKey = 'O2J7GGH2-3NGHUU9J-FHUEH7AW-ITU3UP15'
+
+***REMOVED***
 
 ***REMOVED***
 
@@ -69,9 +72,12 @@ const masterKey = 'O2J7GGH2-3NGHUU9J-FHUEH7AW-ITU3UP15'
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
       ***REMOVED*** else if (error.response.status && error.response.status >= 500) {
 ***REMOVED***
+***REMOVED***
       ***REMOVED*** else {
+***REMOVED***
 ***REMOVED***
       ***REMOVED***
   ***REMOVED***
